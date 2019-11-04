cf3c46d6a9272547f4cb2a6369a5e78ebcce7b9b
igorthedci <foumart@ukr.net>
2019-11-05 01:05:14 +0200

    Homework #08. Task 03 Serialization (in progress)

--- src/main/java/task_03_serialization/TestConteiner.java
+++ src/main/java/task_03_serialization/TestConteiner.java
@@ -1,15 +1,16 @@
 package task_03_serialization;
 
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
+
 /**
  * "3. Написать код, который сериализирует и десериализирует в/из файла все значения полей класса, которые
  * отмечены аннотацией @Save."
  */
 
 @SaveTo(PATH = "roma_file.txt")
-public class TestConteiner {
+public class TestConteiner implements Serializable {
     String text = "text from textContainer";
+    private static final String FILE_NAME = "testConteiner.ser";
 
     @Saver
     public void save(String text1, String path) throws IOException {
@@ -18,6 +19,15 @@ public class TestConteiner {
             w.write(text1);
         } finally {
             w.close();
+        }
+    }
+
+    public static void serialize() {
+        try (FileOutputStream fs = new FileOutputStream(FILE_NAME);
+             ObjectOutputStream os = new ObjectOutputStream(fs)) {
+            os.writeObject(cat);
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
         }
     }
 }
